0d4233b4688861e37bc5627c9338e919daa24411
fsteffen <fsteffen@team.mobile.de>
2016-06-06 09:40:38 +0200

    fixed import

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Editor, Preview } from './index';
+import { Editor, Preview } from '../index';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
